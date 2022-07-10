a8682da35db674d81d366f33b89be085ff3588f0
leekwah <kwah@donga.ac.kr>
2022-07-10 22:45:49 +0900

    20220710 22:45

--- ch04/src/Ex4_8.java
+++ ch04/src/Ex4_8.java
@@ -1,6 +1,9 @@
 class Ex4_8 {
-	public static void main(String args[]) {
-		for (int i = 1; i <= 3; i++) { // 괄호{}안의 문장을 3번 반복
+	public static void main(String args[]) {  
+		// 1부터 3까지 1씩 증가
+		for (int i = 1; i <= 3; i++) {// i = 1, 2, 3
+			// 굳이 1부터 할 필요는 없지만, 숫자를 세기 편하게 하기 위해서 
+			// 괄호{}안의 문장을 3번 반복
 			System.out.println("Hello");
 		}
 	}
