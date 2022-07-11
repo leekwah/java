d3a9da5804827a375047c4cc733e733395ae7ecb
leekwah <kwah@donga.ac.kr>
2022-07-11 22:17:58 +0900

    20220711 22:17

--- ch05/src/Ex5_1.java
+++ ch05/src/Ex5_1.java
@@ -9,11 +9,11 @@ class Ex5_1 {
 		char[] chArr = {'a', 'b', 'c', 'd'};
 
 		for (int i=0; i < iArr1.length ; i++ ) {
-			iArr1[i] = i + 1; // 1~10의 숫자를 순서대로 배열에 넣는다.
+			iArr1[i] = i + 1; // 배열의 초기화를 해서 1~10의 숫자를 순서대로 배열에 넣는다.
 		}
 
 		for (int i=0; i < iArr2.length ; i++ ) {
-			iArr2[i] = (int)(Math.random()*10) + 1; // 1~10의 값을 배열에 저장
+			iArr2[i] = (int)(Math.random()*10) + 1; // 1~10의 난수로 값을 배열에 저장
 		}
 
 		// 배열에 저장된 값들을 출력한다.
@@ -21,11 +21,11 @@ class Ex5_1 {
 			System.out.print(iArr1[i]+",");	
 		}
 		System.out.println();
-
+		// 자주 쓰이는 것들 바로 아래 3문장
 		System.out.println(Arrays.toString(iArr2));
 		System.out.println(Arrays.toString(iArr3));
 		System.out.println(Arrays.toString(chArr));
-		System.out.println(iArr3);
-		System.out.println(chArr);
+		System.out.println(iArr3); // [I@73a28541
+		System.out.println(chArr); // abcd
 	}
 }
