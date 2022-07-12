2143f9e7667208b76e15e592383c198ba10d0e7b
leekwah <kwah@donga.ac.kr>
2022-07-12 22:06:15 +0900

    20220712 22:06

---
+++ ch05/src/Ex5_1_tmp.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Ex5_1_tmp {
+	public static void main(String[] args) {
+		// index : 0~2
+		String[] strArr = {"가위","바위","보"};
+		// 가위 : strArr[0], 바위 : strArr[1], 보 : strArr[2]
+		System.out.println(Arrays.toString(strArr));
+		// random 매소드를 써서 하나가 나오게 하려면?
+		for(int i=0; i<10; i++) {
+			int tmp = (int)(Math.random()*3); // 2가 나온다.
+			System.out.println(strArr[tmp]); // 0~2
+		}
+	}
+}
