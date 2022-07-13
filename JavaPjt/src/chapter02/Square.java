2ca47117120d1a9e4ccd79b908da30d4abeda131
leekwah <lkwnghn@gmail.com>
2022-07-13 17:24:23 +0900

    20220713 17:24

--- JavaPjt/src/chapter02/Square.java
+++ JavaPjt/src/chapter02/Square.java
@@ -5,7 +5,7 @@ public class Square {
 		int n = 4;
 		//2. 메인 메소드에서 밑의 메소드를 호출한다.
 		int s = square(n);
-		System.out.println(s); // 16이 출력된다.
+		System.out.println("한 변의 길이가 "+n+"인 정사각형의 넓이는 "+s+"입니다."); // 16이 출력된다.
 	}
 	//1.여기서 static int는 반환타입이 int라는 뜻이다.
 	//(int length는 매개변수이다.)
