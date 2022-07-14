7d4eb57e8d85e7119c092d542dc6eb7eba5df77e
leekwah <lkwnghn@gmail.com>
2022-07-14 17:44:44 +0900

    20220714 17:44

---
+++ JavaPjt/src/chapter03/LoopExample3.java
@@ -0,0 +1,11 @@
+package chapter03;
+
+public class LoopExample3 {
+	public static void main(String[] args) {
+		int i = 10;
+		
+		do {
+			System.out.println("iÀÇ °ª : " +i);
+		} while (i<3);
+	}
+}
