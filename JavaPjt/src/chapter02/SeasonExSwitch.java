8837a9baea6acdc55ee80159e0fa2c388458bf6f
leekwah <lkwnghn@gmail.com>
2022-07-14 16:22:47 +0900

    20220714 16:22

---
+++ JavaPjt/src/chapter02/SeasonExSwitch.java
@@ -0,0 +1,32 @@
+package chapter02;
+
+import java.util.Scanner;
+
+public class SeasonExSwitch {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("달을 입력하세요(1~12) >> ");
+		int month = scan.nextInt();
+		String season = null;
+		
+		switch (month) {
+			case 3: case 4: case 5:
+				season = "봄";
+				break;
+			case 6: case 7: case 8: 
+				season = "여름";
+				break;
+			case 9: case 10: case 11:   
+				season = "가을"; 
+				break;
+			case 1: case 2: case 12:   
+				season = "겨울"; 
+				break;
+			default: 
+				season = "잘못입력";
+				break;
+		}
+		System.out.println(season);
+		scan.close();
+	}
+}
