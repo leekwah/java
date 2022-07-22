a2e6df738f3977b673d9b760cb18d37d1b631dfb
leekwah <104570042+leekwah@users.noreply.github.com>
2022-07-22 17:43:06 +0900

    20220722 17:43

--- JavaPjt/src/chapter03/InputException.java
+++ JavaPjt/src/chapter03/InputException.java
@@ -13,14 +13,20 @@ public class InputException {
 		
 		for (int i = 0; i < 3; i++) {
 			System.out.print(i+">>");
-			sum += n;
 			try {
 				n = scan.nextInt();
+//				if (n == 9 ) {
+//					scan.next(); // 9를 넣으면 넘김, 그리고 그 다음 것도 넘김
+//					i--; 
+//					continue;
+//				}
 			} catch (InputMismatchException e) {
 				System.out.println("정수가 아닙니다. 다시 입력하세요.");
 				scan.next();
-				continue;
+				i--; // 잘못되었을 때, i--를 해야지 증가되지 않고 계속 되기 때문에 
+				continue; // continue를 주석하면, 앞에 더했던 것들이 두 번 더해지게 된다.
 			}
+			sum += n;
 		}
 		
 		System.out.println("합은 "+sum);
--- Test/src/Test3_Time.java
+++ Test/src/Test3_Time.java
@@ -1,34 +1,25 @@
 import java.util.Calendar;
 
 public class Test3_Time {
-
-	class nowTime {
-		int hourOfDay;
-		int minuteOfDay;
-		void show() {System.out.println("현재 시간은 "+hourOfDay+"시"+ minuteOfDay+"분 입니다.";}
-
-	public Book3() {
-		this("","");
-	}
-		
 	public static void main(String[] args) {
-		Book3 littlePrince2 = new Book3();
-		Book3 littlePrince = new Book3("어린왕자","생텍쥐페리");
-		Book3 emptyBook = new Book3();
-		Book3 loveStory = new Book3("춘향전");
-		littlePrince2.show();
-		littlePrince.show();
-		loveStory.show();
-		emptyBook.show();
-	}
-	
-	public static void main(String[] args) {
-		Book2 littlePrince2 = new Book2();	
 		Calendar now = Calendar.getInstance();
 		int hourOfDay = now.get(Calendar.HOUR_OF_DAY);
 		int minuteOfDay = now.get(Calendar.MINUTE);
 		
-		System.out.println(hourOfDay+"시");
-		System.out.println(minuteOfDay+"분");
+		String sc = "현재 시간은 "+hourOfDay+"시"+minuteOfDay+"분 입니다.\n";
+		String gm = "Good Morning", 
+			   ga = "Good Afternoon",
+			   ge = "Good evening",
+			   gn = "Good Night"; 
+		
+		if (4 <= hourOfDay && hourOfDay < 12) {
+			System.out.println(sc+gm);
+		} else if (12 <= hourOfDay && hourOfDay < 18) {
+			System.out.println(sc+ga);
+		}else if (18 <= hourOfDay && hourOfDay < 22) {
+			System.out.println(sc+ge);
+		} else {
+			System.out.println(sc+gn);
+		}
 	}
 }
